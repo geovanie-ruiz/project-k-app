aeda0f97160318dd85475be1bb2c50ac3d986a5c
Geovanie Ruiz <geovanie.ruiz@gmail.com>
2024-12-13 21:38:37 -0500

    added policy stubs

---
+++ src/app/policies/cookies/page.tsx
@@ -0,0 +1,3 @@
+export default async function Cookies() {
+  return <p>Cookies policy coming soon...</p>;
+}
---
+++ src/app/policies/privacy/page.tsx
@@ -0,0 +1,3 @@
+export default async function Privacy() {
+  return <p>Privacy policy coming soon...</p>;
+}
