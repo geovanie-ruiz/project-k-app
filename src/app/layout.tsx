719ccf54591859f62258a557a1701710a6c377dd
Geovanie Ruiz <geovanie.ruiz@gmail.com>
2024-12-14 20:50:18 -0500

    testing meticulous ai

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,17 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          {(process.env.NODE_ENV === 'development' ||
+            process.env.VERCEL_ENV === 'preview') && (
+            // eslint-disable-next-line @next/next/no-sync-scripts
+            <script
+              data-recording-token="IAdace7dLfv4C47wOrpKACk441eopRwafHEBm1Rb"
+              data-is-production-environment="false"
+              src="https://snippet.meticulous.ai/v1/meticulous.js"
+            />
+          )}
+        </head>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen bg-neutral-100 dark:bg-neutral-900`}
         >
