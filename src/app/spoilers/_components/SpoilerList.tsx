f4785d297bf6f8ebeb0ccb43134a66145cdbb7a6
Geovanie Ruiz <geovanie.ruiz@gmail.com>
2024-12-16 13:24:04 -0500

    remove duplicate import

--- src/app/spoilers/_components/SpoilerList.tsx
+++ src/app/spoilers/_components/SpoilerList.tsx
@@ -2,7 +2,6 @@
 
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
-import { Spoiler } from '../_types/spoiler';
 
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
