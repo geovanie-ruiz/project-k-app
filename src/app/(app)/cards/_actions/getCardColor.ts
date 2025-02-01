6c761aa15f24d8f394ddddc3e38063567115a124
Geovanie Ruiz <geovanie.ruiz@gmail.com>
2025-01-31 23:39:29 -0500

    refactor: enhance getCardColorBG function with type definitions and improved null handling

--- src/app/(app)/cards/_actions/getCardColor.ts
+++ src/app/(app)/cards/_actions/getCardColor.ts
@@ -1,5 +1,11 @@
-export const getCardColorBG = (rune) => {
-  if (rune.length === 0) return "bg-Gray";
+type Runes =
+  | ('Calm' | 'Chaos' | 'Fury' | 'Mental' | 'Order' | 'Physical')[]
+  | null
+  | undefined;
+
+export const getCardColorBG = (rune: Runes) => {
+  if (!rune || rune.length === 0) return 'bg-Gray';
   if (rune.length === 1) return `bg-${rune[0]}`;
-  if (rune.length === 2) return `bg-gradient-to-r from-${rune[0]} to-${rune[1]}`;
+  if (rune.length === 2)
+    return `bg-gradient-to-r from-${rune[0]} to-${rune[1]}`;
 };
